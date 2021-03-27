2e65f2169510bd624d7aab3efdf2b1015af959f4
bhbbbbb <smark9005310@gmail.com>
2021-03-28 00:35:39 +0800

    First Commit

---
+++ about_us/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <core-app-bar />
+
+    <core-drawer />
+
+    <core-view />
+
+    <core-footer />
+
+    <core-cta />
+  </v-app>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+
+    components: {
+      CoreCta: () => import('@/components/core/Cta'),
+      CoreDrawer: () => import('@/components/core/Drawer'),
+      CoreFooter: () => import('@/components/core/Footer'),
+      CoreAppBar: () => import('@/components/core/AppBar'),
+      CoreView: () => import('@/components/core/View'),
+    },
+  }
+</script>
---
+++ about_us/src/components/home/About.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  v-container#about(tag='section')
+    base-subheading About Us
+    p {{ about_us_content }}
+    
+    
+    
+</template>
+
+<script>
+  export default {
+    name: 'HomeAbout',
+
+    data() {
+      return {
+        about_us_content: "some fucking content",
+      }
+    },
+  }
+</script>
