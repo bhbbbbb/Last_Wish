e3b30f1a0c5103a643a5adee944663f9193886bb
fennecJ <hwahwa649@gmail.com>
2021-04-05 02:42:15 +0800

    Implement of  vague layout for about us

--- about_us/src/components/PersonalPage.vue
+++ about_us/src/components/PersonalPage.vue
@@ -1,7 +1,103 @@
-<template lang="pug">
-base-card(@click="$emit('back')", height="682" width="1533" color="rgba(0, 0, 0, 0)")
-    v-img(:src="require(`@/assets/demo.png`)" height="100%")
-        slot/
+<template>
+<base-card @click="$emit('back')" height="682" width="1533" color="rgba(0, 0, 0, 0)">
+  <!--
+  <v-img :src="require(`@/assets/demo.png`)" height="100%">
+  </v-img>
+-->
+
+  <v-container class="grey lighten-5">
+    <v-row>
+      <v-col
+        cols="6"
+        md="4"
+      >
+        <v-card
+          class="pa-2"
+          outlined
+          tile
+          height="50vh"
+        >
+          Photo
+        </v-card>
+      </v-col>
+
+
+      
+      <v-col
+        cols="12"
+        sm="6"
+        md="8"
+      >
+           <v-card
+          shaped
+          class="pa-2"
+          outlined
+          tile
+          height="50vh"
+        >
+          Dream
+        </v-card>
+      </v-col>
+
+      
+    </v-row>
+<v-row>
+  <v-col
+        cols="6"
+        md="4"
+      >
+        <v-card
+          class="pa-2"
+          outlined
+          tile
+        >
+          Name
+        </v-card>
+      </v-col>
+          <v-col
+        cols="12"
+        sm="6"
+        md="8"
+      >
+        <v-card
+          class="pa-2"
+          outlined
+          tile
+        >
+          skills
+        </v-card>
+     </v-col>
+
+      <v-col
+        cols="6"
+        md="4"
+      >
+        <v-card
+          class="pa-2"
+          outlined
+          tile
+        >
+          職位
+        </v-card>
+      </v-col>
+    </v-row>  
+    
+    
+        
+  </v-container>
+
+
+
+
+
+
+
+</base-card>
+
+
+
+
+
 </template>
 
 
@@ -16,3 +112,5 @@ export default {
     }
 }
 </script>
+
+<style> </style>
