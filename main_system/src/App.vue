e2e647c503bb82354d6bff1f24c84cb9dd07aa07
fennecJ <hwahwa649@gmail.com>
2021-05-05 10:55:26 +0800

    Append SimpleUpload for test

--- main_system/src/App.vue
+++ main_system/src/App.vue
@@ -1,23 +1,29 @@
 <template>
   <v-app>
-    <router-view name="AppBar"/>
+    <router-view name="AppBar" />
     <v-main>
-      <router-view name="Main"/>
+      <router-view name="Main" />
     </v-main>
-    <router-view name="Footer"/>
+    <router-view name="Footer" />
     <!-- <AppBarM v-if="this.$vuetify.breakpoint.mobile"/>
     <AppBarD v-else/>
     <router-view style="'padding-top:10vh'"/>
     <FooterM v-if="this.$vuetify.breakpoint.mobile"/> -->
+    <div class="section">
+      <div class="container">
+        <simple-upload />
+      </div>
+    </div>
   </v-app>
 </template>
 
 <script>
+import SimpleUpload from "./components/SimpleUpload.vue";
 
 export default {
-  name: 'App',
-
+  name: "App",
   components: {
+    SimpleUpload,
     // AppBarD: () => import('@/components/AppBar/AppBarD'),
     // AppBarM: () => import('@/components/AppBar/AppBarM'),
     // FooterM: () => import('@/components/FooterM')
