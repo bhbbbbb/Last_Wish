ae70c35464e2ba5405c888759fd27fc60a03e4d4
fennecJ <hwahwa649@gmail.com>
2021-05-09 02:33:25 +0800

    Add apiUserFollowedPosts return as it name in JSON
    Note that the User_id is set to 0 temporarily

--- server/src/router/articles.js
+++ server/src/router/articles.js
@@ -1,4 +1,5 @@
 const articlePATH = __dirname + "/../data/articles.json";
+const accountsPATH = __dirname + "/../data/accounts.json";
 var express = require('express');
 var global = express.Router();
 var user = require('./user.js');
@@ -6,6 +7,7 @@ var fs = require("fs");
 const { stringify } = require('querystring');
 const template = require('../lib/template_maker.js');
 var articles = require(articlePATH);
+var accounts = require(accountsPATH);
 
 var d = new Date();
 var month = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
@@ -69,6 +71,43 @@ global.get('/user_post',(req,res)=>{
     res.json(UserPost);
 }
 )
+
+
+global.get('/followed_post',(req,res)=>{
+    var User_id = String(0);   //Here can be replaced with api returns current user's id
+    var F_User = [];
+    var F_Post = [];
+    var Total_Post = [];
+    for(var i = 0;i<articles.length;i++)
+        if(accounts[i].id === User_id){
+            F_User=accounts[i].followees;
+            F_Post=accounts[i].followedPosts;
+            //console.log(F_User,F_Post,F_User[1]);
+            break;
+        }
+    
+    if(F_User.length)
+    for(var i = 0;i<F_User.length;i++)
+        for(var k in articles){
+            if(articles[k].from==F_User[i])
+                Total_Post.push(articles[k]);
+            console.log(articles[k].from,F_User[i]);
+            }
+
+    if(F_Post.length)
+    for(var j in F_Post)
+        for(var k in articles)
+            if(articles[k].id==F_Post[j])
+                Total_Post.push(articles[k]);
+    console.log(F_User,F_User[0],F_User[1]);
+    console.log(F_Post);
+    console.log(Total_Post);
+    
+    res.json(Total_Post);
+}
+)
+
+
 
 
 global.post('/post', (req, res) => {
