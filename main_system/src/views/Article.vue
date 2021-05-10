f956234b3733a9d49a0f272730f0aae7b616e5ef
fennecJ <hwahwa649@gmail.com>
2021-05-11 01:25:49 +0800

    Implement clone feature

--- main_system/src/views/Article.vue
+++ main_system/src/views/Article.vue
@@ -48,6 +48,7 @@ v-card.ma-3.pa-1(min-height="80vh" rounded="lg" v-if="author" :color="color")
     opacity="0"
   )                              
     v-alert.mt-10(:value="overlay" type="success" transition="slide-x-transition") Copied
+
   v-overlay.align-start(
     :value="overlay2"
     absolute
@@ -56,7 +57,13 @@ v-card.ma-3.pa-1(min-height="80vh" rounded="lg" v-if="author" :color="color")
     v-alert.mt-10(:value="overlay2" type="success" transition="slide-x-transition") Cloned  
   input#url(style="position: absolute; opacity: 0;")
   
-
+  v-overlay.align-start(
+    :value="overlay3"
+    absolute
+    opacity="0"
+  )                              
+    v-alert.mt-10(:value="overlay3" type="error" transition="slide-x-transition") Failed
+  input#url(style="position: absolute; opacity: 0;")
 
   v-text-field.ma-0.pa-1(
     placeholder="comment here"
@@ -69,7 +76,7 @@ v-card.ma-3.pa-1(min-height="80vh" rounded="lg" v-if="author" :color="color")
 
 <script>
 // import { mapState } from 'vuex'
-import {apiUploadComment} from '@/store/api';
+import {apiUploadComment, apiUploadArticle, apiUserPosts} from '@/store/api';
 
 //var Article_id = '';
 
@@ -97,6 +104,7 @@ export default {
     author: undefined,
     overlay: false,
     overlay2: false,
+    overlay3: false,
     Newcomments: '',
   }),
   computed: {
@@ -128,11 +136,43 @@ export default {
       }, 1000);
     },
     Clone() {
+      console.log(this.context);
       //console.log(this.articles[this.id]);
-      this.overlay2 = true;
-      setTimeout(() => {
-        this.overlay2 = false;
-      }, 1000);
+      if (
+        this.$store.state.user_id == '-1' ||
+        this.$store.state.user_id == this.author.id
+        // TODO : Checkout if user have cloned this wishes before
+      ) {
+        this.overlay3 = true;
+        setTimeout(() => {
+          this.overlay3 = false;
+        }, 1000);
+      } else {
+        var new_article = this.context;
+        new_article.from = this.$store.state.user_id;
+        //new_article.push({title: 'QQ'});
+        //Post this article but from is set to user's id
+        apiUploadArticle(new_article)
+          .then((res) => {
+            this.$store.commit('updateGlobalArticles', res.data);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+        //Update user_post state
+        apiUserPosts({username: this.$store.state.username})
+          .then((res) => {
+            this.$store.commit('updateUserArticles', res.data);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+
+        this.overlay2 = true;
+        setTimeout(() => {
+          this.overlay2 = false;
+        }, 1000);
+      }
     },
     SubmitNewComment() {
       apiUploadComment({
