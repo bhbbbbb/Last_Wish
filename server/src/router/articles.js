2d9831b6d5142cbf380922263df7c3e615b0f352
fennecJ <hwahwa649@gmail.com>
2021-05-11 00:35:16 +0800

    Turn back to old method temporaily
    To fix the issue user_post won't update after restart the server

--- server/src/router/articles.js
+++ server/src/router/articles.js
@@ -68,14 +68,15 @@ const USER_POST = [
     }
 ]
 global.post('/user_post', (req, res) => {
-    // // here may simply call getPostsByUser method of account_manager
-    // var User_id = String(-1);   //Here can be replaced with api returns current user's id
-    // var UserPost = [];
-    // for(var i = 0;i<articles.length;i++)
-    //     if(articles[i].from === User_id)
-    //         UserPost.push(articles[i]);
-    // //console.log(UserPost);
-    // res.json(UserPost);
+    // here may simply call getPostsByUser method of account_manager
+    var User_id = accountManager.getIdbyUsername(req.body.username);   //Here can be replaced with api returns current user's id
+    var UserPost = [];
+    for(var i = 0;i<articles.length;i++)
+        if(articles[i].from === User_id)
+            UserPost.push(articles[i]);
+    //console.log(UserPost);
+    res.json(UserPost);
+    /*
     // //TODO: Deal with some exception 
     var response;
     let posts = [];
@@ -91,6 +92,7 @@ global.post('/user_post', (req, res) => {
     response = USER_POST[SUCCEED];
     console.log(posts);
     res.status(response.status).json(posts);
+    */
     return;
 });
 
