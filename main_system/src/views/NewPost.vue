10e0419434d632bc81c5b6ba8920c21ae1ef741b
fennecJ <hwahwa649@gmail.com>
2021-05-13 00:42:31 +0800

    Add some info animation in NewPost page

--- main_system/src/views/NewPost.vue
+++ main_system/src/views/NewPost.vue
@@ -7,10 +7,16 @@ v-card.ma-3.pa-3(min-height="10vh" rounded="lg" elevation="5")
   v-textarea.ma-0.pa-0(
     solo
     auto-grow
-    hint="haha"
+    hint="Tell me about your wish"
     placeholder="body here"
     v-model="new_article.body"
   )
+  v-overlay.align-start(
+    :value="show_info"
+    absolute
+    opacity="0"
+  )                              
+    v-alert.mt-10(:value="show_info" :type="info_type" transition="slide-x-transition") {{infos}}
   v-card-actions.justify-center
     v-btn(@click="SubmitNewArticle()") submit
 </template>
@@ -26,6 +32,9 @@ export default {
       body: '',
       from: '',
     },
+    show_info: false,
+    info_type: 'success',
+    infos: '',
   }),
   computed: {
     ...mapState(['articles']),
@@ -35,6 +44,7 @@ export default {
       this.new_article.from = this.$store.state.user_id;
       if (!this.new_article.title || !this.new_article.body) {
         // todo : error
+        this.Show_info('Blank data!', 'error');
         return;
       }
       apiUploadArticle({
@@ -44,8 +54,10 @@ export default {
         .then((res) => {
           // TODO : insert new post locally
           this.$store.commit('updateGlobalArticles', res.data);
+          this.Show_info('Posted', 'success');
         })
         .catch((err) => {
+          this.Show_info('Something went wrong', 'error');
           console.log(err);
         });
 
@@ -56,6 +68,21 @@ export default {
         .catch((err) => {
           console.log(err);
         });
+    },
+    Show_info(Info, infoType) {
+      /**
+       *There are 4 types of infoType in default:
+       *success
+       *info
+       *warning
+       *error
+       */
+      this.infos = Info;
+      this.info_type = infoType;
+      this.show_info = true;
+      setTimeout(() => {
+        this.show_info = false;
+      }, 1000);
     },
   },
 };
