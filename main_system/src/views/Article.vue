0f987dc7a7675a61128c8080fa2bee42f09b2c4f
fennecJ <hwahwa649@gmail.com>
2021-05-13 02:39:37 +0800

    Use an ugly way to edit an article when cloning

--- main_system/src/views/Article.vue
+++ main_system/src/views/Article.vue
@@ -1,58 +1,41 @@
 <template lang="pug">
-v-card.ma-3.pa-1(min-height="80vh" rounded="lg" v-if="author" :color="color")
+v-card.ma-3.pa-3(v-if="NP" min-height="10vh" rounded="lg" elevation="5")
+  v-text-field.ma-0.pa-1(placeholder="Title here" v-model="new_article.title")
+  v-textarea.ma-0.pa-0(solo="solo" auto-grow="auto-grow" hint="Tell me about your wish" placeholder="body here" v-model="new_article.body")
+  v-card-actions.justify-center
+    v-btn(@click="Clone") submit
+v-card.ma-3.pa-1(v-else="" min-height="80vh" rounded="lg" :color="color")
   v-container
-    v-row.flex-column(no-gutters)
-      v-menu(offset-y close-on-content-click nudge-left="50")
+    v-row.flex-column(no-gutters="no-gutters")
+      v-menu(offset-y="offset-y" close-on-content-click="close-on-content-click" nudge-left="50")
         template(v-slot:activator="{ on, attrs }")
-          v-btn.align-self-end(
-            icon
-            v-bind="attrs"
-            v-on="on"
-          )
+          v-btn.align-self-end(icon="icon" v-bind="attrs" v-on="on")
             v-icon mdi-dots-horizontal
-        v-list(
-
-        )
+        v-list
           v-list-item(@click="Copy") 複製連結
-          v-list-item(@click="Clone") 加到我的清單
-
-    v-row(no-gutters)
+          v-list-item(@click="NPT") 加到我的清單
+    v-row(no-gutters="no-gutters")
       v-col.d-flex.flex-column.flex-shrink-1.align-center.ma-0(cols="4")
         v-avatar.grey.lighten-1(size="64")
         span.py-3.text-center {{ author.username }}
       v-col.d-flex.justify-center.align-center(cols="8")
         h3 {{ context.title }}
-    v-row(no-gutters)
+    v-row(no-gutters="no-gutters")
       v-col.my-0(cols="12")
         v-divider
       v-col.py-2.my-0(cols="auto")
         v-card-subtitle.ma-0.px-4.py-0 {{context.date}}
     v-row
-      v-col.px-8 
-        h3(
-          v-for="wish in context.wishes"
-          :key="wish"
-        ) 
+      v-col.px-8
+        h3(v-for="wish in context.wishes" :key="wish")
           | {{ wish }}
-        br/
+        br
         p.pre {{ context.body }}
-        br/
-        CommentCard(
-          v-for="comment in context.comments"
-          :key="comment.id"
-          :context="comment"
-        )
-    v-overlay.align-start(
-      :value="show_info"
-      absolute
-      opacity="0"
-    )                              
+        br
+        commentcard(v-for="comment in context.comments" :key="comment.id" :context="comment")
+    v-overlay.align-start(:value="show_info" absolute="absolute" opacity="0")
       v-alert.mt-10(:value="show_info" :type="info_type" transition="slide-x-transition") {{infos}}
-
-  v-text-field.ma-0.pa-1(
-    placeholder="comment here"
-    v-model="Newcomments"
-  )
+  v-text-field.ma-0.pa-1(placeholder="comment here" v-model="Newcomments")
   v-card-actions.justify-center
     v-btn(@click="SubmitNewComment()") submit
   input#url(style="position: absolute; opacity: 0;")
@@ -90,6 +73,8 @@ export default {
     info_type: 'success',
     infos: '',
     Newcomments: '',
+    new_article: undefined,
+    NP : false,
   }),
   computed: {
     // ...mapState(['articles']),
@@ -101,6 +86,7 @@ export default {
   created() {
     this.$store.dispatch('getArticle', this.id).then((res) => {
       this.context = res;
+      this.new_article = res;
       //Article_id = this.id;
       this.$store.dispatch('getUser', this.context.from).then((res) => {
         this.author = res;
@@ -117,40 +103,34 @@ export default {
       this.Show_info('Copied', 'success');
     },
     Clone() {
-      // console.log(this.context);
-      //console.log(this.articles[this.id]);
-      if (
-        this.$store.state.user_id == '0' ||
-        this.$store.state.user_id == this.author.id
-        // TODO : Checkout if user have cloned this wishes before
-      ) {
-        this.Show_info('Failed', 'error');
-      } else {
-        var new_article = this.context;
-        new_article.from = this.$store.state.user_id;
+        this.new_article.from = this.$store.state.user_id;
         //new_article.push({title: 'QQ'});
         //Post this article but from is set to user's id
         apiUploadArticle({
           username: this.$store.state.username,
-          article: new_article,
+          article: this.new_article,
         })
           .then((res) => {
-            this.$store.commit('updateGlobalArticles', res.data);
-          })
+          this.$store.commit('updateGlobalArticles', res.data);
+          this.$store.dispatch('getArticle', this.id).then((res) => {
+          this.context = res;
+          this.new_article = res;
+          this.NP = false;
+          });
+            //Update user_post state
+            apiUserPosts({username: this.$store.state.username})
+              .then((res) => {
+                this.$store.commit('updateUserArticles', res.data);
+              })
+              .catch((err) => {
+                console.log(err);
+                this.Show_info('Something went wrong', 'error');
+              });
+        })
           .catch((err) => {
             console.log(err);
           });
-        //Update user_post state
-        apiUserPosts({username: this.$store.state.username})
-          .then((res) => {
-            this.$store.commit('updateUserArticles', res.data);
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-
-        this.Show_info('Cloned', 'success');
-      }
+        this.Show_info('posted', 'success');
     },
     SubmitNewComment() {
       apiUploadComment({
@@ -183,6 +163,9 @@ export default {
       setTimeout(() => {
         this.show_info = false;
       }, 1000);
+    },
+    NPT(){  //New Post True
+      this.NP = true;
     },
   },
 };
