4ae55bc53215a059c89fd4f26589af1d25f574f1
fennecJ <hwahwa649@gmail.com>
2021-05-18 22:48:16 +0800

    Modified pre-commit hook installer

--- do_lint.sh
+++ do_lint.sh
@@ -1,6 +1,13 @@
 echo '
 #/bin/sh
+FILES=$(git diff --cached --name-only --diff-filter=ACMR | sed '"'"'s| |\\\\ |g'"'"')
+[ -z "$FILES" ] && exit 0
+
 cd main_system
 yarn lint
+echo "$FILES" | xargs git add
+
+git update-index -g
+exit 0
 '>.git/hooks/pre-commit
 chmod 775 .git/hooks/pre-commit
