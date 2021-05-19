eceff0b95345b3b312f8035d47dea10caeeebc44
fennecJ <hwahwa649@gmail.com>
2021-05-19 10:45:21 +0800

    Fix wrong path issue

--- do_lint.sh
+++ do_lint.sh
@@ -5,9 +5,15 @@ FILES=$(git diff --cached --name-only --diff-filter=ACMR | sed '"'"'s| |\\\\ |g'
 
 cd main_system
 yarn lint
+cd ../
 echo "$FILES" | xargs git add
-
-git update-index -g
 exit 0
 '>.git/hooks/pre-commit
-chmod 775 .git/hooks/pre-commit
+chmod 777 .git/hooks/pre-commit
+
+echo '
+#!/bin/sh
+git update-index -g
+'>.git/hooks/post-commit
+chmod 777 .git/hooks/post-commit
+exit 0
