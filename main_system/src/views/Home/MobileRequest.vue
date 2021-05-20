43ca0950ceb9d0d2b55ff1a92bd0853440323aff
bhbbbbb <smark9005310@gmail.com>
2021-05-20 15:04:53 +0800

    remove I don't want btn

--- main_system/src/views/Home/MobileRequest.vue
+++ main_system/src/views/Home/MobileRequest.vue
@@ -9,7 +9,7 @@
       Mac (請使用 google chrome) : (Fn+) F12 → Ctrl + Shift + m → 選擇 iPhone X
     </v-card-subtitle>
     <v-card-actions>
-      <v-btn @click="Idontwant">我不依</v-btn>
+      <!-- <v-btn @click="Idontwant">我不依</v-btn> -->
     </v-card-actions>
   </v-card>
 </template>
@@ -18,6 +18,9 @@
 export default {
   name: 'MobileRequest',
   components: {},
+  created() {
+    if (this.$cookies.isKey('mobile')) this.Idontwant();
+  },
   methods: {
     Idontwant() {
       this.$vuetify.breakpoint.mobileBreakpoint = 4000;
