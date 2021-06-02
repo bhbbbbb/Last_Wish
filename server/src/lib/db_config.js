a303e2acbc3d0090e8c5a7756f2c87bea759438e
yuyu <e24076459@gs.ncku.edu.tw>
2021-06-02 17:06:10 +0800

    Update mongoose packages

---
+++ server/src/lib/db_config.js
@@ -0,0 +1,8 @@
+db_config = {
+  'database': 'uidd2021_groupI',
+  'host': 'luffy.ee.ncku.edu.tw:27017',
+  'pasword': '6AW4SJ2VejHCDxEe',
+  'user': 'uidd2021_groupI'
+}
+
+module.exports = db_config;
