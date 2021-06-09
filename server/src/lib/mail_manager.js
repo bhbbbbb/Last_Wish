a6dd91e178fec6c6b8c0078c8e9f25aa08cb61b0
fennecJ <hwahwa649@gmail.com>
2021-06-09 20:53:40 +0800

    Now you can send a mail with generated token

--- server/src/lib/mail_manager.js
+++ server/src/lib/mail_manager.js
@@ -7,34 +7,85 @@ const transporter = nodemailer.createTransport({
     pass: mail_config.pass,
   },
 });
+var status;
 
 module.exports= function(){
     /**
      * @param {String} mailAddr 
-     * @param {String} sub //subject of mail
-     * @param {String} txt //body of mail
+     * @param {String} subject 
+     * @param {String} bodyText
      * @returns 200/401 if mail was sent suc/fail
      */
-    this.sendMails = (mailAddr,sub,txt) => {
+    this.sendMails = (mailAddr,subject,bodyText) => {
+      var mailOptions = {
+      from: 'noreply',
+      to: mailAddr,
+      subject: subject,
+      text: bodyText,
+      };
+      var status = 401;
+     try{
+        transporter.sendMail(mailOptions, function (error, info) {
+          if (error) {
+            throw error;
+          } else {
+            console.log('Email sent: ' + info.response);
+          }
+          })
+        status = 200;
+        }
+      catch(e){
+        status = 401;
+        }
+        return status;
+    }
+
+    /**
+     * @param {String} mailAddr 
+     * @param {String} username
+     * @returns {object} status: 200/401 if mail was sent suc/fail
+     * @returns {object} token: token sent to newUser
+     */
+
+
+    this.sendToken = (mailAddr,username) =>{
+      var token = genNonce(25+Date.now()%6);
+      var text = '哈囉 ' + username +'\n下面是你的驗證碼:\n'+token;
+      var sub = username + ' 這是你的驗證資訊 from lernen';
       var mailOptions = {
       from: 'noreply',
       to: mailAddr,
       subject: sub,
-      text: txt,
+      text: text,
       };
       var status = 401;
-      transporter.sendMail(mailOptions, function(error, info) {
-      if (error) {
-        console.log(error);
-        status = 401;
-      } else {
-        console.log('Email sent: ' + info.response);
+      try{
+        transporter.sendMail(mailOptions, function (error, info) {
+          if (error) {
+            throw error;
+          } else {
+            console.log('Email sent: ' + info.response);
+          }
+        })
         status = 200;
-      }
-      });
-      return status;
+        }catch(e){
+          status = 401;
+        }
+      return {'token':token,'status':status};
     }
+
+
 }
 
 
 
+function genNonce(length) {
+    let result           = [];
+    let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result.push(
+          characters.charAt(Math.floor(Math.random() * charactersLength)));
+   }
+   return result.join('');
+}
