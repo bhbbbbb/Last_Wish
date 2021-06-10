e7e00add6cb81f8617282c7a699c5338bb7badb6
fennecJ <hwahwa649@gmail.com>
2021-06-10 09:31:54 +0800

    Fix several bugs caused by formatter

--- server/src/lib/mail_manager.js
+++ server/src/lib/mail_manager.js
@@ -1,106 +1,104 @@
 var nodemailer = require('nodemailer');
 var mail_config = require('./mail_config');
-//const User = require('../models/User');
+// const User = require('../models/User');
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: mail_config.user,
-    pass: mail_config.pass,
-  },
+    service : 'gmail',
+    auth : {
+        user : mail_config.user,
+        pass : mail_config.pass,
+    },
 });
 var status;
 
-module.exports = function () {
-  // /**
-  //  * @param {Srting} mailAddr
-  //  * @returns if mailAddr is already in list
-  //  * @throws any error happened
-  //  */
-  // this.hasMailAddr = async function (mailAddr) {
-  //   try {
-  //     return await User.findOne({email: mailAddr})
-  //       .exec()
-  //       .then((addr) => {
-  //         return addr != null;
-  //       });
-  //   } catch (error) {
-  //     throw error;
-  //   }
-  // };
+module.exports = function() {
+    // /**
+    //  * @param {Srting} mailAddr
+    //  * @returns if mailAddr is already in list
+    //  * @throws any error happened
+    //  */
+    // this.hasMailAddr = async function (mailAddr) {
+    //   try {
+    //     return await User.findOne({email: mailAddr})
+    //       .exec()
+    //       .then((addr) => {
+    //         return addr != null;
+    //       });
+    //   } catch (error) {
+    //     throw error;
+    //   }
+    // };
 
-  /**
-   * @param {String} mailAddr
-   * @returns if mailAddr is a valid email address
-   */
+    /**
+     * @param {String} mailAddr
+     * @returns if mailAddr is a valid email address
+     */
 
-  this.isValidAddr = (mailAddr) => {
-    return isValidAddr(mailAddr);
-  };
+    this.isValidAddr = (mailAddr) => { return isValidAddr(mailAddr) };
 
-  /**
-   * @param {String} mailAddr
-   * @param {String} subject
-   * @param {String} bodyText
-   * @returns 200/401 if mail was sent suc/fail
-   * @throws any error happened
-   */
+    /**
+     * @param {String} mailAddr
+     * @param {String} subject
+     * @param {String} bodyText
+     * @returns 200/401 if mail was sent suc/fail
+     * @throws any error happened
+     */
 
-  this.sendMails = (mailAddr, subject, bodyText) => {
-    var mailOptions = {
-      from: 'noreply',
-      to: mailAddr,
-      subject: subject,
-      text: bodyText,
-    };
-    var status = 401;
-    try {
-      transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-          throw error;
-        } else {
-          console.log('Email sent: ' + info.response);
+    this.sendMails = (mailAddr, subject, bodyText) => {
+        var mailOptions = {
+            from : 'noreply',
+            to : mailAddr,
+            subject : subject,
+            text : bodyText,
+        };
+        var status = 401;
+        try {
+            transporter.sendMail(mailOptions, function(error, info) {
+                if (error) {
+                    throw error;
+                } else {
+                    console.log('Email sent: ' + info.response);
+                }
+            });
+            status = 200;
+        } catch (e) {
+            status = 401;
         }
-      });
-      status = 200;
-    } catch (e) {
-      status = 401;
-    }
-    return status;
-  };
-
-  /**
-   * @param {String} mailAddr
-   * @param {String} username
-   * @returns {object} status: 200/401 if mail was sent suc/fail
-   * @returns {object} token: token sent to newUser
-   * @throws any error happened
-   */
-
-  this.sendToken = (mailAddr, username) => {
-    var token = genNonce(25 + (Date.now() % 6));
-    var text = '哈囉 ' + username + '\n下面是你的驗證碼:\n' + token;
-    var sub = username + ' 這是你的驗證資訊 from lernen';
-    var mailOptions = {
-      from: 'noreply',
-      to: mailAddr,
-      subject: sub,
-      text: text,
+        return status;
     };
-    var status = 401;
-    try {
-      transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-          throw error;
-        } else {
-          console.log('Email sent: ' + info.response);
+
+    /**
+     * @param {String} mailAddr
+     * @param {String} username
+     * @returns {object} status: 200/401 if mail was sent suc/fail
+     * @returns {object} token: token sent to newUser
+     * @throws any error happened
+     */
+
+    this.sendToken = (mailAddr, username) => {
+        var token = genNonce(25 + (Date.now() % 8));
+        var text = '哈囉 ' + username + '\n下面是你的驗證碼:\n' + token;
+        var sub = username + ' 這是你的驗證資訊 from lernen';
+        var mailOptions = {
+            from : 'noreply',
+            to : mailAddr,
+            subject : sub,
+            text : text,
+        };
+        var status = 401;
+        try {
+            transporter.sendMail(mailOptions, function(error, info) {
+                if (error) {
+                    throw error;
+                } else {
+                    console.log('Email sent: ' + info.response);
+                }
+            });
+            status = 200;
+        } catch (e) {
+            status = 401;
         }
-      });
-      status = 200;
-    } catch (e) {
-      status = 401;
-    }
-    return {token: token, status: status};
-  };
+        return {'token' : token, 'status' : status};
+    };
 };
 
 /**
@@ -108,20 +106,22 @@ module.exports = function () {
  * @returns A string of random chars with length = input
  */
 function genNonce(length) {
-  let result = [];
-  let characters =
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let charactersLength = characters.length;
-  for (var i = 0; i < length; i++) {
-    result.push(
-      characters.charAt(Math.floor(Math.random() * charactersLength))
-    );
-  }
-  return result.join('');
+    let result = [];
+    let characters =
+        'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+        result.push(
+            characters.charAt(Math.floor(Math.random() * charactersLength)));
+    }
+    return result.join('');
 }
 
 function isValidAddr(mailAddr) {
-  var pattern =
-    "A[a-z0-9!#$%&'*+/=?^_‘{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_‘{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?z";
-  return mailAddr.match(pattern);
+    const pattern =
+        /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    // new RegExp(
+    //  "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+(?:[A-Z]{2}|com|org|net|gov|mil|biz|info|mobi|name|aero|jobs|museum)\b");
+    console.log(pattern.test(mailAddr.toLowerCase()));
+    return pattern.test(mailAddr.toLowerCase());
 }
