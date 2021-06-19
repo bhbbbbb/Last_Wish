43282528c3deb8f1a7529a251db113bc426ca9d0
fennecJ <hwahwa649@gmail.com>
2021-06-19 11:13:30 +0800

    Now sever will del old pic when user set new pic

--- server/src/router/upload.js
+++ server/src/router/upload.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var uploads = express.Router();
 const FilePath = __dirname + '/../data/uploads';
+var user_session = require('../lib/session.js');
+const account_manager = require("../lib/account_manager");
+const accountManager = new account_manager();
+var fs = require('fs');
+
 function genNonce(length) {
     let result = [];
     let characters =
@@ -13,15 +18,29 @@ function genNonce(length) {
     return result.join('');
 }
 
+async function deleteFile(filename){
+    let path = FilePath + '/' + filename.split('/media/')[1];
+    if(!filename){
+        console.log('No_old_pic');
+        return;
+    }
+    try {
+        await fs.unlink(path, function (err) {});
+    } catch (error) {
+        console.log(error);
+        return; // do nothing if err occur;
+    }
+}
+
 // File upload part
 var multer = require('multer');
 var storage = multer.diskStorage({
     destination : function(req, file, callback) { callback(null, FilePath); },
     filename : function(req, file, callback) {
-        console.log(file);
+        //console.log(file);
         callback(
             null,
-            Date.now() + genNonce(Date.now() % 6) +
+            Date.now() + genNonce(Date.now() % 6 + 5) +
                 ".jpg"); // The file.originalname can be change to any string
                          // you want. While if you want to change to other name,
                          // you need to append Filename Extension, too.
@@ -30,15 +49,25 @@ var storage = multer.diskStorage({
 
 const upload = multer({storage : storage}).single('file');
 
-uploads.post('/uploadFile',
-             async (req, res) => {upload(req, res, function(err) {
-                 if (err) {
-                     console.log(err)
-                 } else {
-                     var FileName = req.file.filename;
-                     res.status(200).send(FileName);
-                 }
-             })});
+uploads.post('/uploadFile', user_session, async (req, res) => 
+{
+    let userId = req.session.user_id;
+    try {
+        let user = await accountManager.getUserInfo(userId);
+        if(user)
+            await deleteFile(user.proPic);
+        upload(req, res, function(err) {
+            if (err) {
+                console.log(err)
+                } else {
+                    var FileName = req.file.filename;
+                    res.status(200).send(FileName);
+                }
+        })
+    } catch (error) {
+        res.status(400).json('user not found');
+    }
+});
 
 // File upload part
 
