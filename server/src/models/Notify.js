826fba59db80811434f3ac1d435cbfd74a0f6203
fennecJ <hwahwa649@gmail.com>
2021-06-22 20:21:37 +0800

    Use enum instead of validator to limit notify type

--- server/src/models/Notify.js
+++ server/src/models/Notify.js
@@ -1,16 +1,13 @@
 const mongoose = require("mongoose");
 
-const typeValidator = (t) => {
-    valid_type = ['Post','Comment','Followed','Liked'];
-    return (t in valid_type);
-}
+
 
 const notifySchema = new mongoose.Schema({
   from: {type: mongoose.Types.ObjectId},
   to: {type: mongoose.Types.ObjectId},
   Actions: {
     type: String,
-    validate: typeValidator,
+    enum:['Post','Comment','Followed','Liked'],
   },
   date: {
     type: Date,
