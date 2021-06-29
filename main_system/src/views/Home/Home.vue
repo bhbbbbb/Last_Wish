5673586ce6bc647a448e96e6c58d1abdc88f07ea
bhbbbbb <smark9005310@gmail.com>
2021-06-29 17:03:12 +0800

    remove moblileRequest

--- main_system/src/views/Home/Home.vue
+++ main_system/src/views/Home/Home.vue
@@ -2,8 +2,9 @@
 v-container.pa-0.align-start.justify-center(fill-height)
   v-row.justify-center.mar(style="width: 90vw;")
     v-col.pa-0.ma-0(cols="12" sm="12" md="10")
-      router-view(v-if="$vuetify.breakpoint.mobile")/
-      MobileRequest(v-else)
+      router-view
+      //- router-view(v-if="$vuetify.breakpoint.mobile")/
+      //- MobileRequest(v-else)
 </template>
 
 <script>
