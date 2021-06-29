589b5563bdbf3d6acd7c96db718a4af815f2985b
yuyu <e24076459@gs.ncku.edu.tw>
2021-06-29 15:18:53 +0800

    Improve db config, connect to different db

--- server/db.config.js
+++ server/db.config.js
@@ -1,6 +1,22 @@
 module.exports  = {
-  'database': 'uidd2021_groupI',
-  'host': 'luffy.ee.ncku.edu.tw',
-  'password': '6AW4SJ2VejHCDxEe',
-  'user': 'uidd2021_groupI'
+  'luffy': {
+    'database': 'uidd2021_groupI',
+    'host': 'luffy.ee.ncku.edu.tw',
+    'password': '6AW4SJ2VejHCDxEe',
+    'user': 'uidd2021_groupI'
+  },
+  'test': {
+    'database': 'test',
+    'host': 'cluster0-shard-00-00.jhbg0.mongodb.net:27017,cluster0-shard-00-01.jhbg0.mongodb.net:27017,cluster0-shard-00-02.jhbg0.mongodb.net:27017',
+    'password': 'isengineer',
+    'user': 'yuyu',
+    'postfix': 'ssl=true&replicaSet=atlas-qfbkre-shard-0&authSource=admin&retryWrites=true&w=majority'
+  },
+  'alpha': {
+    'database': 'alpha',
+    'host': 'cluster0-shard-00-00.jhbg0.mongodb.net:27017,cluster0-shard-00-01.jhbg0.mongodb.net:27017,cluster0-shard-00-02.jhbg0.mongodb.net:27017',
+    'password': 'isengineer',
+    'user': 'yuyu',
+    'postfix': 'ssl=true&replicaSet=atlas-qfbkre-shard-0&authSource=admin&retryWrites=true&w=majority'
+  }
 };
